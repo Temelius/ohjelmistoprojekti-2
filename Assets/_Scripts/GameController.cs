32d32cb36c88b6efb2b595d2b0ab28a754fddd1d
joonasrom <70328782+joonasrom@users.noreply.github.com>
2021-11-22 16:16:28 +0200

    Update GameController.cs

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -54,27 +54,19 @@ public class GameController : MonoBehaviour
         {
             saveData = tempData;
         }
-
-        
     }
 
     public void OnTriggerEnter(Collider coll)
     {
-        
         StartCoroutine(waitples());
         skipidii = GetComponent<AudioSource>();
         skipidii.Play();
-        
     }
 
     IEnumerator waitples()
     {
-        
         transition.SetTrigger("Start");
-        
         yield return new WaitForSeconds(3);
-
-        
         SceneManager.LoadScene(GoToSceneNumber);
     }
 
@@ -87,7 +79,4 @@ public class GameController : MonoBehaviour
             saveData.SaveToDisk();
         }
     }
-
-    
-
 }
